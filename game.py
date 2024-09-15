f18e88e4ae29f71dc0e242e344867b8bac320d6d
Promn21 <suppawit.sontarat@gmail.com>
2024-09-15 15:23:00 +0700

    version3

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame
 import random
 WIDTH=1280
 HEIGHT=720
+MAX_SPEED = 8
 
 class Agent:
     def __init__(self, x, y) -> None:
@@ -13,6 +14,8 @@ class Agent:
 
     def update(self):
         self.velocity = self.velocity + self.acceleration
+        if self.velocity.length() > MAX_SPEED:
+            self.velocity = self.velocity.normalize() * MAX_SPEED
         self.position = self.position + self.velocity
         self.acceleration = pygame.Vector2(0,0)
 
@@ -20,7 +23,26 @@ class Agent:
         force = pygame.Vector2(x,y)
         self.acceleration = self.acceleration + (force/self.mass)
 
+    def seek(self, x,y):
+        dir = player - self.position #seeking direction
+        dir= dir.normalize() * 0.2
+        seeking_force = dir
+        self.apply_force(seeking_force.x, seeking_force.y)
+
+   # def coherence(self, agents):
+    #    center_of_mass = pygame.Vector2(0, 0)
+        
+     #   for agent in agents:
+      #      if agent != self:
+       #         center_of_mass += agent.position
+        #center_of_mass /= len(agents) - 1 
+
+        #dir = center_of_mass - self.position
+        #f = dir.normalize() *0.1
+        #self.apply_force(f.x, f.y) 
+
     def draw(self):
+        pygame.draw.circle(screen,"white",  self.position, 20, width=2) #for silly white outline to make the agent cooller idk
         pygame.draw.circle(screen,"red", self.position, 10)
 
 agents = []
@@ -35,6 +57,7 @@ clock = pygame.time.Clock()
 
 
 #----- player editors -----
+player = pygame.Vector2()
 x = 50
 y = 50
 #width = 40
@@ -55,16 +78,16 @@ while running:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
-        x -= velocity
+        player.x -= velocity
     
     if keys[pygame.K_d]:
-        x += velocity
+        player.x += velocity
     
     if keys[pygame.K_w]:
-        y -= velocity
+        player.y -= velocity
    
     if keys[pygame.K_s]:
-        y += velocity
+        player.y += velocity
     
     
 
@@ -73,9 +96,11 @@ while running:
 
     # RENDER YOUR GAME HERE
     ###pygame.draw.circle(screen, "black", [640,360], 15)
-    pygame.draw.circle(screen,("black"), (x, y), radius) 
+    pygame.draw.circle(screen,("black"), (player.x, player.y), radius) 
     
     for agent in agents:
+            agent.seek(400,400)
+            #agent.coherence(agents)
             agent.update()
             agent.draw()
 
